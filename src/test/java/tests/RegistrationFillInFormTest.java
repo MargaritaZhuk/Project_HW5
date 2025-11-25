2ba513f5d62abd412ddabd55cd61d25a9bec8398
Margarita Zhuk <zhukrita@gmail.com>
2025-11-25 19:43:23 +0300

    Add code for jenkins

--- src/test/java/tests/RegistrationFillInFormTest.java
+++ src/test/java/tests/RegistrationFillInFormTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
@@ -15,41 +16,68 @@ public class RegistrationFillInFormTest extends TestBase {
 
     @Test
     @Tag("registration")
+    @DisplayName("Заполнение формы полными данными")
     void fillTextFormTest() {
         step("Open form", () -> {
-            registrationPage.openPage();
+            registrationPage.openPage().removeBanners();
         });
         step("Fill in form", () -> {
-            registrationPage.removeBanners().setFirstName("Margarita").setLastName("Zhuk").setEmail("zhukrita@gmail.com").setGender("Female").setPhone("9663453928").setDateOfBirth("1997", "March", "5").setSubjects("M").setHobbies("Sports").setPicture("image1.jpeg").setAddress("Address 1").setState("NCR").setCity("Delhi").submitForm();
+            registrationPage.setFirstName("Margarita").setLastName("Zhuk").setEmail("zhukrita@gmail.com").setGender("Female").setPhone("9663453928").setDateOfBirth("1997", "March", "5").setSubjects("M").setHobbies("Sports").setPicture("image1.jpeg").setAddress("Address 1").setState("NCR").setCity("Delhi").submitForm();
         });
         step("Check results", () -> {
-        resultModalComponent.checkModalIsOpen().checkResult("Student Name", "Margarita Zhuk").checkResult("Student Email", "zhukrita@gmail.com").checkResult("Gender", "Female").checkResult("Mobile", "9663453928").checkResult("Date of Birth", "05 March,1997").checkResult("Subjects", "Maths").checkResult("Hobbies", "Sports").checkResult("Picture", "image1.jpeg").checkResult("Address", "Address 1").checkResult("State and City", "NCR Delhi").closeModal();
+            resultModalComponent.checkModalIsOpen().checkResult("Student Name", "Margarita Zhuk").checkResult("Student Email", "zhukrita@gmail.com").checkResult("Gender", "Female").checkResult("Mobile", "9663453928").checkResult("Date of Birth", "05 March,1997").checkResult("Subjects", "Maths").checkResult("Hobbies", "Sports").checkResult("Picture", "image1.jpeg").checkResult("Address", "Address 1").checkResult("State and City", "NCR Delhi").closeModal();
         });
     }
 
     @Test
     @Tag("registration")
     @Disabled("Require some changes")
+    @DisplayName("Заполнение формы минимальными данными")
     void fillMinimalFormTest() {
-        registrationPage.openPage().removeBanners().setFirstName("Margarita").setLastName("Zhuk").setGender("Female").setPhone("9663453928").setDateOfBirth("1997", "March", "5").submitForm();
+        step("Open form", () -> {
+            registrationPage.openPage().removeBanners();
+        });
 
-        resultModalComponent.checkModalIsOpen().checkResult("Student Name", "Margarita Zhuk").checkEmptyResult("Student Email").checkResult("Gender", "Female").checkResult("Mobile", "9663453928").checkResult("Date of Birth", "05 March,1997").checkEmptyResult("Subjects").checkEmptyResult("Hobbies").checkEmptyResult("Picture").checkEmptyResult("Address").checkEmptyResult("State and City").closeModal();
+        step("Fill in form", () -> {
+            registrationPage.setFirstName("Margarita").setLastName("Zhuk").setGender("Female").setPhone("9663453928").setDateOfBirth("1997", "March", "5").submitForm();
+        });
 
+        step("Check results", () -> {
+            resultModalComponent.checkModalIsOpen().checkResult("Student Name", "Margarita Zhuk").checkEmptyResult("Student Email").checkResult("Gender", "Female").checkResult("Mobile", "9663453928").checkResult("Date of Birth", "05 March,1997").checkEmptyResult("Subjects").checkEmptyResult("Hobbies").checkEmptyResult("Picture").checkEmptyResult("Address").checkEmptyResult("State and City").closeModal();
+        });
     }
 
     @Test
     @Tag("registration")
+    @DisplayName("Валидация обязательных полей")
     void validationTextFormTest() {
         step("Open form", () -> {
-            registrationPage.openPage().removeBanners().submitForm().checkOverallValidation().checkFirstNameValidation().checkLastNameValidation().checkGenderValidation().checkPhoneValidation();});
-        resultModalComponent.checkModalIsNotOpen();
+            registrationPage.openPage().removeBanners();
+        });
+        step("Submit form", () -> {
+            registrationPage.submitForm();
+        });
+        step("Check results", () -> {
+            registrationPage.checkOverallValidation().checkFirstNameValidation().checkLastNameValidation().checkGenderValidation().checkPhoneValidation();
+            resultModalComponent.checkModalIsNotOpen();
+        });
     }
 
     @Test
     @Tag("registration")
+    @DisplayName("Валидация эл.почты - заведомо провальный")
     void emailValidationTest() {
-        registrationPage.openPage().removeBanners().setFirstName("Margarita").setLastName("Zhuk").setGender("Female").setPhone("9663453928").setEmail("aaa@aa").submitForm().checkEmailValidation();
-        resultModalComponent.checkModalIsNotOpen();
+        step("Open form", () -> {
+            registrationPage.openPage().removeBanners();
+        });
+
+        step("Fill in form", () -> {
+            registrationPage.setFirstName("Margarita").setLastName("Zhuk").setGender("Female").setPhone("9663453928").setEmail("aaa@gmail.com").submitForm();
+        });
+        step("Check results", () -> {
+            registrationPage.checkEmailValidation();
+            resultModalComponent.checkModalIsNotOpen();
+        });
     }
 }
 
