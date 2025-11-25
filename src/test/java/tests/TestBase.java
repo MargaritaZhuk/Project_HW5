54106788ccbbae0e113c4f16392319ad52eb3195
Margarita Zhuk <zhukrita@gmail.com>
2025-11-25 22:56:29 +0300

    Add code for properties

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,11 +14,13 @@ import java.util.Map;
 public class TestBase {
     @BeforeAll
     static void setupEnvironment() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = System.getProperty("baseURL", "https://demoqa.com");
+        Configuration.browserSize = System.getProperty("browserSize","1920x1080");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "142.0");
         Configuration.timeout = 10000;
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = System.getProperty("REMOTE_BROWSER_URL","https://user1:1234@selenoid.autotests.cloud/wd/hub");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
